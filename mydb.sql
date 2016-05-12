8f1f506cd89afd1d95359f705306f861933d9366
wasuremono <kenneth.s.k.ng@gmail.com>
2016-05-12 23:41:37 +1000

    Updated bookingOrder and booking schema

--- mydb.sql
+++ mydb.sql
@@ -1,5 +1,3 @@
-CREATE DATABASE  IF NOT EXISTS `mydb` /*!40100 DEFAULT CHARACTER SET utf8 */;
-USE `mydb`;
 -- MySQL dump 10.13  Distrib 5.7.9, for Win64 (x86_64)
 --
 -- Host: localhost    Database: mydb
@@ -18,103 +16,51 @@ USE `mydb`;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `bookingorders`
+-- Dumping data for table `bookingorders`
 --
 
-DROP TABLE IF EXISTS `bookingorders`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `bookingorders` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `checkin` varchar(10) NOT NULL,
-  `checkout` varchar(10) NOT NULL,
-  `uid` int(11) NOT NULL,
-  `roomType` varchar(20) NOT NULL,
-  `extraBed` bit(1) NOT NULL,
-  `bookingDate` varchar(10) NOT NULL,
-  PRIMARY KEY (`id`,`uid`),
-  KEY `uid_idx` (`uid`),
-  CONSTRAINT `idBookingOrderbookingordersUser` FOREIGN KEY (`uid`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
+LOCK TABLES `bookingorders` WRITE;
+/*!40000 ALTER TABLE `bookingorders` DISABLE KEYS */;
+/*!40000 ALTER TABLE `bookingorders` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
--- Table structure for table `bookings`
+-- Dumping data for table `bookings`
 --
 
-DROP TABLE IF EXISTS `bookings`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `bookings` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `checkin` varchar(10) NOT NULL,
-  `checkout` varchar(10) NOT NULL,
-  `uid` int(11) NOT NULL,
-  `roomType` varchar(20) NOT NULL,
-  `extraBed` bit(1) NOT NULL,
-  `bookingDate` varchar(10) NOT NULL,
-  PRIMARY KEY (`id`,`uid`),
-  KEY `uid_idx` (`uid`),
-  CONSTRAINT `idBookingUser` FOREIGN KEY (`uid`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
+LOCK TABLES `bookings` WRITE;
+/*!40000 ALTER TABLE `bookings` DISABLE KEYS */;
+/*!40000 ALTER TABLE `bookings` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
--- Table structure for table `hotels`
+-- Dumping data for table `hotels`
 --
 
-DROP TABLE IF EXISTS `hotels`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `hotels` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `preview` varchar(200) DEFAULT NULL,
-  `city` varchar(50) DEFAULT NULL,
-  `owner` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
+LOCK TABLES `hotels` WRITE;
+/*!40000 ALTER TABLE `hotels` DISABLE KEYS */;
+/*!40000 ALTER TABLE `hotels` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
--- Table structure for table `rooms`
+-- Dumping data for table `rooms`
 --
 
-DROP TABLE IF EXISTS `rooms`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `rooms` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `price` int(11) NOT NULL,
-  `hotel_id` int(11) DEFAULT NULL,
-  `roomType` varchar(20) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
+LOCK TABLES `rooms` WRITE;
+/*!40000 ALTER TABLE `rooms` DISABLE KEYS */;
+INSERT INTO `rooms` VALUES (1,80,1,'1');
+/*!40000 ALTER TABLE `rooms` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
--- Table structure for table `users`
+-- Dumping data for table `users`
 --
 
-DROP TABLE IF EXISTS `users`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `username` varchar(50) NOT NULL,
-  `password` varchar(50) NOT NULL,
-  `firstName` varchar(30) DEFAULT NULL,
-  `lastName` varchar(30) DEFAULT NULL,
-  `nickName` varchar(30) DEFAULT NULL,
-  `address` varchar(50) DEFAULT NULL,
-  `email` varchar(30) DEFAULT NULL,
-  `cardNumber` varchar(16) DEFAULT NULL,
-  `cardName` varchar(30) DEFAULT NULL,
-  `cardExpire` datetime DEFAULT NULL,
-  `cvc` int(11) DEFAULT NULL,
-  `userType` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'kngx286','hasaki','Kenneth','Ng','Ken',NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -125,4 +71,4 @@ CREATE TABLE `users` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2016-05-12 20:47:15
+-- Dump completed on 2016-05-12 23:41:06
